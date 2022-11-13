66badd8439ecf04529dd06ce71ced5aa8a53fe89
cabrera.luc <cabreralucaspatricio@gmail.com>
2022-11-12 22:52:10 -0300

    last3

--- src/components/Trash.jsx
+++ src/components/Trash.jsx
@@ -8,6 +8,8 @@ import Cards from "./Cards"
 import styles from "./ModulesCss/Trash.module.css"
 import { Link } from "react-router-dom"
 import { useState } from "react"
+import Swal from 'sweetalert2'
+
 
 export default function Trash(){
 
@@ -27,57 +29,95 @@ useEffect(()=>{
 function aplicarCambios(){
     deleteFromTrash(stateDelete.lista)
     restoreFromTrash(stateRestore.lista)
-    setTimeout(() => {
-        dispatch(getTrash())
-    }, 3000);
+   
     
     setStateRestore({lista:[]})
     setStateDelete({lista:[]})
     
 }
 
-function handlerSubirAlStateDelete(element){
-    if(!stateRestore.lista.includes(element.name)){
-        if(!stateDelete.lista.includes(element.name)){
-            setStateDelete({...stateDelete,lista:[...stateDelete.lista, element.name]})
+function handlerDelete(element){
+    Swal.fire({
+        title: 'Are you want to delete permanently this pokemon?',
+        showCancelButton: true,
+        confirmButtonText: 'Yes',
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+  
+            deleteFromTrash([element.name])
+           
+            let timerInterval
+Swal.fire({
+  title: 'Auto close alert!',
+  html: 'Deleting',
+  timer: 2000,
+  timerProgressBar: false,
+  didOpen: () => {
+    dispatch(getTrash())
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+    window.location.reload()
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+            
+         
         }
-        if(stateDelete.lista.includes(element.name)){
-            setStateDelete({
-                ...stateDelete,lista:stateDelete.lista.filter(e => e!==element.name)
-            })
-        }
-    }
-    if(stateRestore.lista.includes(element.name)){
-        setStateRestore({
-            ...stateRestore,lista:stateRestore.lista.filter(e => e!==element.name)
-        })
-        if(!stateDelete.lista.includes(element.name)){
-            setStateDelete({...stateDelete,lista:[...stateDelete.lista, element.name]})
-        }
-    }
+      })
+   
+   
 }
 
-function handlerSubirAlStateRestore(element){
-    if(!stateDelete.lista.includes(element.name)){
-        if(!stateRestore.lista.includes(element.name)){
-            setStateRestore({...stateRestore,lista:[...stateRestore.lista, element.name]})
+function handlerRestore(element){
+    Swal.fire({
+        title: 'Are you want to restore this pokemon?',
+        showCancelButton: true,
+        confirmButtonText: 'Yes',
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+  
+            restoreFromTrash([element.name])
+          
+            let timerInterval
+            Swal.fire({
+                title: 'Auto close alert!',
+                html: 'Restoring',
+                timer: 2000,
+                timerProgressBar: false,
+                didOpen: () => {
+                  dispatch(getTrash())
+                  Swal.showLoading()
+                  const b = Swal.getHtmlContainer().querySelector('b')
+                  timerInterval = setInterval(() => {
+                    b.textContent = Swal.getTimerLeft()
+                  }, 100)
+                },
+                willClose: () => {
+                  clearInterval(timerInterval)
+                  window.location.reload()
+                }
+              }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                  console.log('I was closed by the timer')
+                }
+              })
+         
         }
-        if(stateRestore.lista.includes(element.name)){
-            setStateRestore({
-                ...stateRestore,lista:stateRestore.lista.filter(e => e!==element.name)
-            })
-        }
-        
-    }
-    if(stateDelete.lista.includes(element.name)){
-        
-        setStateDelete({
-            ...stateDelete,lista:stateDelete.lista.filter(e => e!==element.name)
-        })
-        if(!stateRestore.lista.includes(element.name)){
-            setStateRestore({...stateRestore,lista:[...stateRestore.lista, element.name]})
-        }
-    }
+      })
+   
 }
 
 
@@ -88,7 +128,6 @@ return (
          <Link to ="/MainPage">
          <button className={styles.buttonsHeader}>Return to main page</button>
          </Link>
-         <button className={styles.buttonsHeader} onClick={()=>{aplicarCambios()}} >Apply changes</button>
         </div>
       <div className={styles.body}>
       
@@ -105,7 +144,10 @@ return (
                         <div>
                            
                         <div className={styles.card}>
-                            <img className={styles.img} src={element.img} alt="pokemon_img" />
+
+                            <div className={styles.img_box}>
+                             <img className={styles.img} src={element.img} alt="pokemon_img" />
+                            </div>
 
                         <div className={styles.card_data}>
                             <h3 className={styles.name} >{element.name}</h3>
@@ -113,33 +155,18 @@ return (
                         </div>
                         
 
-                            {stateDelete.lista.includes(element.name)?
-                            ( <div className={styles.buttons_card}> <button onClick={()=>{
-                                handlerSubirAlStateDelete(element)
-                                    }} className={styles.button_delete_pressed} ><VscClose></VscClose></button>
-                                    <button onClick={()=>{
-                                    handlerSubirAlStateRestore(element)
-                                    }} className={styles.button_restore}><VscDebugRestart/></button>
-                                    </div> )
-                            :stateRestore.lista.includes(element.name)?
-                            ( <div className={styles.buttons_card}> <button onClick={()=>{
-                                handlerSubirAlStateDelete(element)
-                                    }} className={styles.button_delete}><VscClose></VscClose></button>
-                                    <button onClick={()=>{
-                                    handlerSubirAlStateRestore(element)
-                                    }} className={styles.button_restore_pressed}><VscDebugRestart/></button>
-                                    </div> )
-                            :
+                        
+                             <div className={styles.buttons_card}> 
+                              <button onClick={()=>{
+                                handlerDelete(element)
+                                    }} className={styles.button_delete_pressed} ><VscClose/>
+                               </button>
+                               <button onClick={()=>{
+                                    handlerRestore(element)
+                                    }} className={styles.button_restore}><VscDebugRestart/>
+                                </button>
+                              </div> 
                             
-                            
-                            (<div className={styles.buttons_card}>
-                                <button onClick={()=>{
-                                handlerSubirAlStateDelete(element)
-                                    }} className={styles.button_delete}><VscClose></VscClose></button>
-                                    <button onClick={()=>{
-                                    handlerSubirAlStateRestore(element)
-                                    }} className={styles.button_restore}><VscDebugRestart/></button>  </div> )
-                            }
                      
                        
                       
